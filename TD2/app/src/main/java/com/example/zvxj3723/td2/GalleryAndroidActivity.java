31cded7a2cfe091c36250d5887e9915d6205bb2b
ltb-bossuyt <luongthibien.bossuyt@gmail.com>
2017-01-24 14:06:19 +0100

    Exo 1 TD2

---
+++ TD2/app/src/main/java/com/example/zvxj3723/td2/GalleryAndroidActivity.java
@@ -0,0 +1,26 @@
+package com.example.zvxj3723.td2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Gallery;
+import android.widget.Toast;
+
+public class GalleryAndroidActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gallery);
+
+        Gallery g = (Gallery) findViewById(R.id.gallery);
+        g.setAdapter(new ImageAdapter(this));
+        g.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ TD2/app/src/main/java/com/example/zvxj3723/td2/ImageAdapter.java
@@ -0,0 +1,58 @@
+package com.example.zvxj3723.td2;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.SpinnerAdapter;
+
+/**
+ * Created by ZVXJ3723 on 24/01/2017.
+ */
+public class ImageAdapter extends BaseAdapter {
+
+    int mGalleryItemBackground;
+    private Context mContext;
+    private Integer[] mImageIds = {
+            R.drawable.donut,
+            R.drawable.eclair,
+            R.drawable.lollipop,
+            R.drawable.nougat,
+    };
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+        TypedArray a = mContext.obtainStyledAttributes(R.styleable.GalleryAndroid);
+        mGalleryItemBackground = a.getResourceId(R.styleable.GalleryAndroid_android_galleryItemBackground, 0);
+        a.recycle();
+    }
+
+    @Override
+    public int getCount() {
+        return mImageIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView i = new ImageView(mContext);
+        i.setImageResource(mImageIds[position]);
+        i.setLayoutParams(new Gallery.LayoutParams(150, 170));
+        i.setScaleType(ImageView.ScaleType.FIT_XY);
+        i.setBackgroundResource(mGalleryItemBackground);
+
+        return i;
+    }
+}
